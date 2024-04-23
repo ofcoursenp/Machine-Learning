fda29b6012b62c3c28d39c9d004b2a3ea63bd479
ofcoursenp <nexusalt33@gmail.com>
2024-04-23 22:28:44 +0545

    A little Matplotlib twist

--- Logistic regression/logistic_regression.py
+++ Logistic regression/logistic_regression.py
@@ -1,6 +1,10 @@
 from sklearn import datasets
 from sklearn.linear_model import LogisticRegression
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Virginical flower detection
 
 iris = datasets.load_iris()
 
@@ -15,3 +19,15 @@ clf = LogisticRegression()
 clf.fit(x,y)
 
 print(clf.predict([[0.3]]))
+
+
+
+# Advance Logistic Regression
+x_new = np.linspace(0,3,1000).reshape(-1,1)
+# print(x_new)
+
+y_prob = clf.predict_proba(x_new)
+# print(y_prob)
+
+plt.plot(x_new,y_prob[:,1],'g-',label='virginica')
+plt.show()
